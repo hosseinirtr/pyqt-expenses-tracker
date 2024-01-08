8d6b873f620f7c7b71b73c21ad52f26774727109
Seyed Hossein Hosseini <65307359+hosseinirtr@users.noreply.github.com>
2024-01-08 12:10:16 +0330

    Update test_project.py

--- test_project.py
+++ test_project.py
@@ -1,7 +1,7 @@
 import pytest
 from project import add_user, add_transaction, get_transactions, get_expenses, get_income, remove_user
 
-def setup_function(function):
+def setup_function():
     # Add a user for testing
     remove_user('TestUser')
     user_id, _ = add_user('TestUser')
@@ -9,7 +9,7 @@ def setup_function(function):
 
 def test_add_transaction():
     # Add a transaction and check if it was added correctly
-    user_id = setup_function(add_user)
+    user_id = setup_function()
     add_transaction(user_id, 100.0, True,'Test Transaction',)
     transactions = get_transactions(user_id)
     assert len(transactions) == 1
@@ -18,7 +18,7 @@ def test_add_transaction():
 
 def test_get_expenses():
     # Add an expense and check if it's returned by get_expenses
-    user_id = setup_function(add_user)
+    user_id = setup_function()
     add_transaction(user_id, 50.0, False, 'Test Expense')
     expenses = get_expenses(user_id)
     assert len(expenses) == 1
@@ -26,7 +26,7 @@ def test_get_expenses():
 
 def test_get_income():
     # Add an income and check if it's returned by get_income
-    user_id = setup_function(add_user)
+    user_id = setup_function()
     add_transaction(user_id, 200.0, True, 'Test Income')
     incomes = get_income(user_id)
     assert len(incomes) == 1
